0ed861ec440fa8b0c569932041f9ba3d0588d26e
Антон <a.shabalinski@gmail.com>
2020-09-21 08:47:05 +0300

    Добавьте файлы проекта.

---
+++ trying/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using addSubMulDiv;
+
+namespace trying
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите 2 числа: ");
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(Class1.Add(a, b));
+            Console.WriteLine(Class1.Sub(a, b));
+            Console.WriteLine(Class1.Mul(a, b));
+            Console.WriteLine(Class1.Div(a, b));
+        }
+    }
+}
